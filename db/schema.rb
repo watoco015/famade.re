c289a41dd439dfde5fcd323a6272e22d2af2b682
watoco015 <fuwari015@gmail.com>
2023-11-10 14:32:10 +0900

    art model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_10_051949) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_10_052808) do
+  create_table "arts", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_arts_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -24,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_051949) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "arts", "users"
 end

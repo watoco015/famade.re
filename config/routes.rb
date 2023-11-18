d338df5939d5b8d0648a39e8a75f48ff0adf4920
watoco015 <fuwari015@gmail.com>
2023-11-18 16:34:17 +0900

    model controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'arts#index'
-  resources :arts
+  resources :arts do
+    resources :comments, only: :create
+  end
   resources :users
 end

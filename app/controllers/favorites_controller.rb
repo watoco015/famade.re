6286314d1fd69a0801b4502be76ca086dfcd6e57
watoco015 <fuwari015@gmail.com>
2023-11-19 14:27:26 +0900

    favo_c　修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,22 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_favorite, only: [:create, :destroy]
 
   def create
     favorite = current_user.favorites.build(art_id: params[:art_id])
     favorite.save
-    redirect_to arts_path
+    redirect_to art_path(@art.id)
   end
 
   def destroy
     favorite = Favorite.find_by(art_id: params[:art_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to arts_path
+    redirect_to art_path(@art.id)
+  end
+
+  private
+
+  def set_favorite
+    @art = Art.find(params[:art_id])
   end
 end

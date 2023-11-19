031afccf540af31aed09034a11fd221cfc970422
watoco015 <fuwari015@gmail.com>
2023-11-19 13:30:12 +0900

    アソシエーション追加

--- app/models/art.rb
+++ app/models/art.rb
@@ -9,4 +9,9 @@ class Art < ApplicationRecord
 
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :favorites
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end

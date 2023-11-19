ef6451f39a8a403a0a8386ed4663a9aaea47d679
watoco015 <fuwari015@gmail.com>
2023-11-19 13:19:32 +0900

    update変更

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,6 +1,6 @@
 class ArtsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_art, only: [:show, :edit, :destroy]
+  before_action :set_art, only: [:show, :edit, :destroy, :update]
   before_action :authorize_user!, only: [:edit, :update, :destroy]
 
   def index
@@ -34,9 +34,8 @@ class ArtsController < ApplicationController
   end
 
   def update
-    art = Art.find(params[:id])
-    art.update(art_params)
-    redirect_to art_path(art.id)
+    @art.update(art_params)
+    redirect_to art_path(@art.id)
   end
 
   private
